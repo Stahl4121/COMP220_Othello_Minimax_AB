fca506cefc244957d2b1ae490f70c00890f28058
Nimitz <MumawBM1@gcc.edu>
2017-04-07 12:50:25 -0400

    added get statements for everything

--- src/Board.java
+++ src/Board.java
@@ -1,6 +1,16 @@
 
 public class Board {
-	public Board(){
-		
+	public char[][] board;
+	
+	public Board(){			//original
+		board=new char[8][8];
+	}
+	
+	public Board(Board board){
+		for(int r=0;r<8;r++){
+			for(int c=0;c<8;c++){
+				this.board[r][c]= other.board[r][c];
+			}
+		}
 	}
 }
--- src/Move.java
+++ src/Move.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 /**
  * Position on board
  * @author MUMAWBM1
@@ -6,12 +8,27 @@
 public class Move {
 	private int row;
 	private int col;
-	String foo;
+	private ArrayList<Integer> coor;//coor(0)=row; coor(1)=col;
 	
 	int Foo;
 	
 	public Move(int r,int c){
 	this.row=r;
 	this.col=c;
+	coor=new ArrayList<>();
+	coor.add(row);
+	coor.add(col);
+	}
+	
+	public int getRow(){
+		return row;
+	}
+	
+	public int getCol(){
+		return col;
+	}
+	
+	public ArrayList<Integer> getCoor(){
+		return coor;
 	}
 }
