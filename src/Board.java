e0837dc2d78f36a556b59ec7cf53011ff605d3bb
Nimitz <MumawBM1@gcc.edu>
2017-04-07 15:04:00 -0400

    I'm in my room if'n you wanna come work here.
    Rudiments of Board class, needs much improvement

--- src/Board.java
+++ src/Board.java
@@ -6,10 +6,18 @@ public class Board {
 		board=new char[8][8];
 	}
 	
-	public Board(Board board){
+	public Board(Board other){
 		for(int r=0;r<8;r++){
 			for(int c=0;c<8;c++){
 				this.board[r][c]= other.board[r][c];
+			}
+		}
+	}
+	
+	public void flipPieces(Move move){
+		for(int r=move.getRow()-1;r<=(move.getRow()+1);r++){
+			for(int c=move.getCol()-1;c<=(move.getCol()+1);c++){
+				//Im in my room if'n you wanna come and work in the same room.
 			}
 		}
 	}
