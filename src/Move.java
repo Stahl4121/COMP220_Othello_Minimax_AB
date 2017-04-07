2ba0be79ddcee1e3bca211c9c504b978111d0f08
Nimitz <MumawBM1@gcc.edu>
2017-04-07 12:16:53 -0400

    Coodinate position of a move.

--- src/Move.java
+++ src/Move.java
@@ -1,4 +1,14 @@
-
+/**
+ * Position on board
+ * @author MUMAWBM1
+ *
+ */
 public class Move {
-
+	private int row;
+	private int col;
+	
+	public Move(int r,int c){
+	this.row=r;
+	this.col=c;
+	}
 }
