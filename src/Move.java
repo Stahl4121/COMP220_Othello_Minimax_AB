d42d3ccb3227ba68ad5e4514adcfb4f7d0486437
Stahl4121 <logan316stahl@gmail.com>
2017-04-07 16:25:46 -0400

    Added SquareStatus member variable to Move

--- src/Move.java
+++ src/Move.java
@@ -6,12 +6,14 @@
 public class Move {
 	private int row;
 	private int col;
+	private SquareStatus piece;
 
 
 	
-	public Move(int r,int c){
+	public Move(int r,int c, SquareStatus piece){
 	this.row=r;
 	this.col=c;
+	this.piece=piece;
 
 	}
 	
@@ -21,6 +23,10 @@ public class Move {
 	
 	public int getCol(){
 		return col;
+	}
+	
+	public SquareStatus getColor(){
+		return piece;
 	}
 
 }
