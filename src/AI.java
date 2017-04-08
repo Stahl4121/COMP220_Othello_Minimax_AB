9eb890175acc0968f71b11986596f059d1491443
Stahl4121 <logan316stahl@gmail.com>
2017-04-07 22:49:38 -0400

    Revert "Revert "Wrote Testing Scenario in Main""
    This reverts commit 03a3d597efe9843db17c571332bf0c7afb465d3f.

--- src/AI.java
+++ src/AI.java
@@ -3,52 +3,52 @@ import java.util.Collections;
 
 public class AI {
 
-	int depth=8;
-	
-	public Move aiMove(Board current){
-		max(current,0);
-	}
-
-	public int max(Board iter, int n){
-		if(n==depth){
-			return iter.numTilesOfColor(SquareStatus.WHITE);
-		}
-		Board copy = new Board(iter);
-		ArrayList<Integer> moves = new ArrayList<>();
-		for(int r=0;r<8;r++){
-			for(int c=0;c<8;c++){
-				if (copy.board[r][c]==SquareStatus.EMPTY){
-					Move move = new Move(r,c,SquareStatus.WHITE);
-					if(copy.isLegalMove(move)){
-						copy.makeMove(move);
-						moves.add(min(copy,n++));
-					}
-				}
-			}
-		}
-		Collections.sort(moves);
-		return moves.get(moves.size()-1);
-	}
-	
-	public int min(Board iter, int n){
-		if(n==depth){
-			return iter.numTilesOfColor(SquareStatus.EMPTY);
-		}
-		Board copy = new Board(iter);
-		ArrayList<Integer> moves = new ArrayList<>();
-		for(int r=0;r<8;r++){
-			for(int c=0;c<8;c++){
-				if (copy.board[r][c]==SquareStatus.EMPTY){
-					Move move = new Move(r,c,SquareStatus.WHITE);
-					if(copy.isLegalMove(move)){
-						copy.makeMove(move);
-						return(min(copy,n++));
-					}
-				}
-			}
-		}
-		Collections.sort(moves);
-		return moves.get(0);
-	}
+//	int depth=8;
+//	
+//	public Move aiMove(Board current){
+//		max(current,0);
+//	}
+//
+//	public int max(Board iter, int n){
+//		if(n==depth){
+//			return iter.numTilesOfColor(SquareStatus.WHITE);
+//		}
+//		Board copy = new Board(iter);
+//		ArrayList<Integer> moves = new ArrayList<>();
+//		for(int r=0;r<8;r++){
+//			for(int c=0;c<8;c++){
+//				if (copy.board[r][c]==SquareStatus.EMPTY){
+//					Move move = new Move(r,c,SquareStatus.WHITE);
+//					if(copy.isLegalMove(move)){
+//						copy.makeMove(move);
+//						moves.add(min(copy,n++));
+//					}
+//				}
+//			}
+//		}
+//		Collections.sort(moves);
+//		return moves.get(moves.size()-1);
+//	}
+//	
+//	public int min(Board iter, int n){
+//		if(n==depth){
+//			return iter.numTilesOfColor(SquareStatus.EMPTY);
+//		}
+//		Board copy = new Board(iter);
+//		ArrayList<Integer> moves = new ArrayList<>();
+//		for(int r=0;r<8;r++){
+//			for(int c=0;c<8;c++){
+//				if (copy.board[r][c]==SquareStatus.EMPTY){
+//					Move move = new Move(r,c,SquareStatus.WHITE);
+//					if(copy.isLegalMove(move)){
+//						copy.makeMove(move);
+//						return(min(copy,n++));
+//					}
+//				}
+//			}
+//		}
+//		Collections.sort(moves);
+//		return moves.get(0);
+//	}
 	
 }
