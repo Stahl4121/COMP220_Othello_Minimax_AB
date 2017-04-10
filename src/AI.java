5ec8190f52215e53c69c1dea6608dc86c1d88af0
Nimitz <MumawBM1@gcc.edu>
2017-04-10 19:27:26 -0400

    Java Doc'd

--- src/AI.java
+++ src/AI.java
@@ -1,21 +1,52 @@
 import java.util.HashMap;
 import java.util.Map;
-
+/**
+ * This Class creates an ai opponent for a human to play against. The AI goes second
+ * @author MUMAWBM1
+ *
+ */
 public class AI {
 
 	private int depth;
+	/**
+	 * Default constructor for an AI object.
+	 * 
+	 * It sets depth to 8.
+	 */
+	public AI(){
+		depth=8;
+	}
 	
+	
+	/**
+	 * This is a constructor for an AI object
+	 * @param depth	this tells the ai how many moves into the future it should look.
+	 */
 	public AI(int depth){
 		this.depth=depth;
 	}
 	
+	/**
+	 * 
+	 * @return Returns how many moves into the future the ai is calculating.
+	 */
 	public int getDepth(){
 		return depth;
 	}
+	
+	/**
+	 * 
+	 * @param Allows user to set how many moves into the future the AI calculates
+	 */
 	public void setDepth(int d){
 		depth=d;
 	}
 	
+	/**
+	 * 
+	 * @param current Takes in the current state of the board to figure out how many
+	 * @return Returns the most optimal move to be played by the AI
+	 */
 	public Move aiMove(Board current){
 		Move m= new Move();
 		HashMap<Move, Integer> move = new HashMap<Move, Integer>();
@@ -32,13 +63,22 @@ public class AI {
 		
 	}
 
-	public Map<Move, Integer> max(Board iter, int n , Move m){
+	/**
+	 * 
+	 * @param iterated	BBoard Object passed from max. It is a projected move possibility
+	 * @param n		Iteration counter for recursion
+	 * @param m		Move Object that allows the min/max chain to keep track of move to pass up chain
+	 * @return		Returns a Map that uses a Move object as the key and an 
+	 * 				Integer value as the value. This is how all pertinent information is relayed
+	 * 				back up to aiMove.
+	 */
+	public Map<Move, Integer> max(Board iterated, int n , Move m){
 		if(n==depth){
 			HashMap<Move, Integer> score = new HashMap<Move,Integer>();
-			score.put(m, iter.getNumTiles(SquareStatus.WHITE));
+			score.put(m, iterated.getNumTiles(SquareStatus.WHITE));
 			return score;
 		}
-		Board copy = new Board(iter);
+		Board copy = new Board(iterated);
 		Map<Move, Integer> currentMax = new HashMap<Move, Integer>();
 		if(n==0){
 			currentMax.put(m, -9999999);
@@ -85,13 +125,23 @@ public class AI {
 		return currentMax;
 	}
 
-	public Map<Move, Integer> min(Board iter, int n, Move m){
+	
+	/**
+	 * 
+	 * @param iterated 	Board Object passed from max. It is a projected move possibility
+	 * @param n			Keeps track of recursion layer
+	 * @param m			Move Object that allows the min/max chain to keep track of move to pass up chain
+	 * @return			Returns a Map that uses a Move object as the key and an 
+	 * 				Integer value as the value. This is how all pertinent information is relayed
+	 * 				back up to aiMove.
+	 */
+	public Map<Move, Integer> min(Board iterated, int n, Move m){
 		if(n==depth){
 			HashMap<Move, Integer> score = new HashMap<Move, Integer>();
-			score.put(m, iter.getNumTiles(SquareStatus.WHITE));
+			score.put(m, iterated.getNumTiles(SquareStatus.WHITE));
 			return score;
 		}
-		Board copy = new Board(iter);
+		Board copy = new Board(iterated);
 		Map<Move, Integer> currentMin = new HashMap<Move, Integer>();
 		Move currentMinMove=new Move(m);
 		for(int r=0;r<copy.BOARD_SIZE;r++){
