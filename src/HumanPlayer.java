41ea745ba293a73034f0b475fb762cbaf0ccc3e5
Nimitz <MumawBM1@gcc.edu>
2017-04-10 21:35:41 -0400

    This is a human

---
+++ src/HumanPlayer.java
@@ -0,0 +1,17 @@
+
+public class HumanPlayer {
+	/**
+	 * creates scanner to read in player's move
+	 */
+	public void getInput(){
+		
+	}
+	/**
+	 * Allows player to start/stop/pause game and/or reset
+	 */
+	public void commands(){
+		
+	}
+	
+	
+}
